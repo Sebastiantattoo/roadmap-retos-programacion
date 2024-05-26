2eeb4d11b0be893499dbf86341db30fcd03836f6
Sebastiantattoo <sebastiantattoventas@gmail.com>
2024-05-26 20:06:16 -0300

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Sebastiantattoo.js
@@ -0,0 +1,33 @@
+// Este es un comentario en una sola linea
+/**
+ * En cambio este es un comentario
+ * en varias lineas.
+ * otra.
+ * otra.
+ */
+
+//la direccion de internet que mas se acerca para javascript es:
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Vamos a crear variables en Java Script carajo!!!
+
+const titleOfLanguageOfProgramming = "El lenguaje elejido es JS.";
+const pi = 3.1416;
+var myString = "Este es un tipo de dato string";
+var myNumber = 369;
+var myBooleanOne = true;
+var myBooleanTwo = false;
+var myUndefinedVar = undefined;
+let mybigInt = 12345678912345678n;
+let myNullVar = null;
+let myFloatNumber = 36.9;
+
+document.write (titleOfLanguageOfProgramming, "<br>", myBooleanOne, "<br>", myBooleanTwo,"<br>",
+    myNumber, "<br>", myString, "<br>", myUndefinedVar, "<br>", mybigInt, "<br>", myNullVar, "<br>", myFloatNumber );
+
+console.log ("Arrancamos con Javascript" + ". " + titleOfLanguageOfProgramming);
+
+
+//Ejercicio nro uno terminado.
+
+    
